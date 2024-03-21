ac92ba8c652a6eb78007655191104fdb8220d7f1
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-03-21 19:09:38 -0300

    Adição das imagens e rem

---
+++ lista-alongamento.js
@@ -0,0 +1,47 @@
+[
+    {
+        "id": 1,
+        "nome": "Alongamento de braços",
+        "desc": "Alonga braços e ombros, aliviando tensão.",
+        "execucao": "Levante um braço à altura do peito, cruzando-o à frente do corpo. Com a outra mão, pressione o cotovelo para sentir o alongamento. Mantenha por 15 segundos e troque de braço.",
+        "calorias": 5,
+        "dificuldade": "Baixa",
+        "img": "assets/alongamento-bracos.png"
+    },
+    {
+        "id": 2,
+        "nome": "Alongamento de pescoço",
+        "desc": "Relaxa os músculos do pescoço.",
+        "execucao": "Incline a cabeça em direção ao ombro, aplicando leve pressão com a mão. Segure por 15 segundos e troque de lado.",
+        "calorias": 3,
+        "dificuldade": "Baixa",
+        "img": "assets/alongamento-pescoco.png"
+    },
+    {
+        "id": 3,
+        "nome": "Torção de coluna",
+        "desc": "Melhora a flexibilidade da coluna.",
+        "execucao": "Sentado, torça o tronco para o lado, apoiando a mão no joelho oposto. Olhe para trás, segure por 15 segundos e troque de lado.",
+        "calorias": 4,
+        "dificuldade": "Média",
+        "img": "assets/torcao-coluna.png"
+    },
+    {
+        "id": 4,
+        "nome": "Elevação de quadril",
+        "desc": "Alivia tensão nos quadris e lombar.",
+        "execucao": "Deite-se de costas, cruze uma perna sobre a outra, incline-se para o lado contrário da perna cruzada, estendendo o braço acima da cabeça. Mantenha a posição por 15 segundos e depois troque de lado, repetindo o movimento.",
+        "calorias": 6,
+        "dificuldade": "Média",
+        "img": "assets/elevacao-quadril.png"
+    },
+    {
+        "id": 5,
+        "nome": "Alongamento de panturrilha",
+        "desc": "Previne cãibras e aumenta a flexibilidade da panturrilha.",
+        "execucao": "Com uma perna à frente e outra atrás, pressione o calcanhar de trás no chão. Incline-se para frente para intensificar o alongamento. Mantenha por 15 segundos e troque de perna.",
+        "calorias": 5,
+        "dificuldade": "Baixa",
+        "img": "assets/alongamento-panturrilha.png"
+    }
+]
--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 let startTimer = document.querySelector(".fa-solid")
-
-startTimer.addEventListener("click", ()=>{
-
+startTimer.addEventListener("click", () => {
   startTimer.classList.toggle("fa-pause")
   startTimer.classList.toggle("fa-play")
 })
 
 let section = document.querySelector("section")
-
 let button = document.querySelector(".fa-child-reaching")
 
-button.addEventListener("click", ()=>{
- section.classList.toggle("hidden")
-
-
-}
-)
-
+button.addEventListener("click", () => {
+  section.classList.toggle("hidden")
+})
