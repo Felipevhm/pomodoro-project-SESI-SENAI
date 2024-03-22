ad07002fb0e87ae5bf52247071b572c656c14bab
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-03-22 00:10:15 -0300

    Layout finalizado, falta somente inserir a funcionalidade de escolher randomincamente uma atividade de alongamento ao término do tempo

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const playButton = document.querySelector(".iniciar i");
 const counterElement = document.querySelector(".visor p");
 const btnApper = document.querySelector('.fa-child-reaching');
 const section = document.querySelector('section')
+const checkBtn = document.querySelector('.check-button')
+const timerAlongamento = document.querySelector('timer-choose')
 
 let countdown;
-let timeRemaining = 25 * 60;
+let timeRemaining = 0.05 * 60;
 
 function updateCounter() {
     let minutes = Math.floor(timeRemaining / 60);
@@ -23,6 +25,8 @@ function toggleTimer() {
             if (timeRemaining > 0) {
                 timeRemaining--;
                 updateCounter();
+            } else if (timeRemaining == 0){
+                section.classList.remove('hidden')
             } else {
                 clearInterval(countdown);
             }
@@ -42,19 +46,44 @@ btnApper.addEventListener('click', () => {
   section.classList.toggle('hidden')
 })
 
+let BtnStop = document.querySelector('.button-stop')
+BtnStop.addEventListener('click', () => {
+    clearInterval(countdown);
+    counterElement.innerHTML = '0:03'
+    timeRemaining = 0.05 * 60;
+
+    if (playButton.classList.contains('fa-pause')){
+        playButton.classList.remove('fa-pause')
+        playButton.classList.add('fa-play')
+    }
+})
+
 const checkButton = document.querySelector('.check-button')
+let isClicked = false;
 checkButton.addEventListener('mouseover', () =>{
-    checkButton.classList.remove('fa-circle');
-    checkButton.classList.add('fa-circle-check')
-})
+    if(!isClicked){
+        checkButton.classList.remove('fa-circle');
+        checkButton.classList.add('fa-circle-check')
+        timerAlongamento.classList.add('bg-finished-alongamento')
 
+    }
+})
 checkButton.addEventListener('mouseout', () =>{
-    checkButton.classList.remove('fa-circle-check');
-    checkButton.classList.add('fa-circle')
+    if(!isClicked){
+        checkButton.classList.remove('fa-circle-check');
+        checkButton.classList.add('fa-circle')
+    }
 })
-
 checkButton.addEventListener('click', () => {
-    
-    checkButton.classList.add('fa-circle-')
-    checkButton.style.color = 'green'
+    isClicked = true;
+    checkButton.classList.add('fa-circle-check');
+    checkButton.classList.remove('fa-circle');
+    checkButton.classList.add('fa-solid');
+    checkButton.style.color = 'rgba(255, 255, 255, 0.474);'
+
+    section.classList.add('hidden')
+    counterElement.innerHTML = "0:03"
+    timeRemaining = 0.05 * 60
+    checkButton.classList.remove('fa-solid');
+    isClicked = false;
 })
