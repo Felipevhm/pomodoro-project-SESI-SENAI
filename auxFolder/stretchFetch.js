b25811de2fe6527b58b01626b4b81020238a5a3d
Felipe <fvhmanhaes@gmail.com>
2024-03-21 22:20:18 -0300

    Add fetch from API ninjas exercises endpoint

---
+++ auxFolder/stretchFetch.js
@@ -0,0 +1,33 @@
+const apiKey = '1fZ4NS89JhTbeeXvEu8Uhg==zWf7ioezJ86uDQ3u'
+
+async function getExercises() {
+
+   let options = {
+   method: 'GET',
+   headers: { 'x-api-key': apiKey }
+}
+ 
+   let url =  'https://api.api-ninjas.com/v1/exercises?type=stretching'
+ 
+
+    let data;
+    try {
+        const response = await fetch(url, options);
+        data = await response.json();
+    } catch (err) {
+        console.log(`error ${err}`);
+    }
+    return data;
+}
+
+// const batata = await getExercises()
+// .then(receivedData => {
+//     return receivedData;
+// });
+
+async function main() {
+   const output = await getExercises();
+   console.log(output);
+}
+
+ main();
