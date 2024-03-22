77a45a2ff9aef8c22a582b92a2b4f8cec1fee0ac
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-03-21 22:20:54 -0300

    Processo do dia 21/03

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
-let startTimer = document.querySelector(".fa-solid")
-startTimer.addEventListener("click", () => {
-  startTimer.classList.toggle("fa-pause")
-  startTimer.classList.toggle("fa-play")
+const playButton = document.querySelector(".iniciar i");
+const counterElement = document.querySelector(".visor p");
+const btnApper = document.querySelector('.fa-child-reaching');
+const section = document.querySelector('section')
+
+let countdown;
+let timeRemaining = 25 * 60;
+
+function updateCounter() {
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = timeRemaining % 60;
+    counterElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+function toggleTimer() {
+    if (playButton.classList.contains("fa-play")) {
+        playButton.classList.remove("fa-play");
+        playButton.classList.add("fa-pause");
+        if (counterElement.textContent.trim() === "25:00") {
+            timeRemaining = 25 * 60;
+        }
+        countdown = setInterval(() => {
+            if (timeRemaining > 0) {
+                timeRemaining--;
+                updateCounter();
+            } else {
+                clearInterval(countdown);
+            }
+        }, 1000);
+    } else {
+        // Pausar o contador
+        playButton.classList.add("fa-play");
+        playButton.classList.remove("fa-pause");
+        clearInterval(countdown);
+    }
+}
+
+playButton.addEventListener("click", toggleTimer);
+updateCounter(); 
+
+btnApper.addEventListener('click', () => {
+  section.classList.toggle('hidden')
 })
 
-let section = document.querySelector("section")
-let button = document.querySelector(".fa-child-reaching")
+const checkButton = document.querySelector('.check-button')
+checkButton.addEventListener('mouseover', () =>{
+    checkButton.classList.remove('fa-circle');
+    checkButton.classList.add('fa-circle-check')
+})
 
-button.addEventListener("click", () => {
-  section.classList.toggle("hidden")
+checkButton.addEventListener('mouseout', () =>{
+    checkButton.classList.remove('fa-circle-check');
+    checkButton.classList.add('fa-circle')
+})
+
+checkButton.addEventListener('click', () => {
+    
+    checkButton.classList.add('fa-circle-')
+    checkButton.style.color = 'green'
 })
